a53f148363837b744b9192d0b7d163f5a1a01332
hanvithakotagiri <144478343+hanvithakotagiri@users.noreply.github.com>
2024-08-14 14:45:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    changeNavbarStyle();
+};
+
+function changeNavbarStyle() {
+    var navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.style.backgroundColor = "#222";
+    } else {
+        navbar.style.backgroundColor = "#333";
+    }
+}
